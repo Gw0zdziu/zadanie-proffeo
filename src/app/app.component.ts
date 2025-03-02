1e071ce1d9e17c993635d9b675df6ca1d22277bf
gw0zdziu <gw0zdziu09@gmail.com>
2025-03-02 19:18:08 +0100

    feat(view): add feature sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {OmdbapiService} from '../shared/apis/omdbapi/omdbapi.service';
-import {NgOptimizedImage} from '@angular/common';
 import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {IMovie} from '../shared/models/movie';
@@ -12,33 +11,61 @@ import {
   MatHeaderCellDef,
   MatHeaderRow,
   MatHeaderRowDef,
+  MatNoDataRow,
   MatRow,
   MatRowDef,
   MatTable,
   MatTableDataSource
 } from '@angular/material/table';
-import {MatPaginator} from '@angular/material/paginator';
+import {FormsModule} from '@angular/forms';
+import {MatSort, MatSortHeader, Sort} from '@angular/material/sort';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [MatFormField, MatInput, MatLabel, MatTable, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRow, MatRowDef, MatPaginator, NgOptimizedImage],
+  imports: [MatFormField, MatInput, MatLabel, MatTable, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRow, MatRowDef, FormsModule, MatNoDataRow, MatSort, MatSortHeader, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   dataTable = new MatTableDataSource<IMovie>([])
-  columns: string[] = ['poster', 'title', 'type', 'year'];
+  columns: string[] = ['Poster', 'Title', 'Type', 'Year'];
+  filterInput: any;
+  errorResponse: string = '';
+  response: string = '';
+  @ViewChild(MatSort) matSort: MatSort;
 
-  constructor(private omdbapiService: OmdbapiService) {
-  }
+  constructor(private omdbapiService: OmdbapiService) {}
 
   ngOnInit() {
+
+
   }
 
-  searchMovies(e: Event){
-    const inputValue = (e.target as HTMLInputElement).value;
-    this.omdbapiService.getMovies(inputValue).subscribe(x => {
-      this.dataTable.data = x.Search;
+  ngAfterViewInit() {
+    this.dataTable.sort = this.matSort;
+  }
+
+  searchMovies(event: Event){
+    this.filterInput = [];
+    this.dataTable.filter = '';
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.omdbapiService.getMovies(inputValue.trim().toLowerCase()).subscribe(x => {
+      if (x.Response === 'True'){
+        this.dataTable.data = x.Search;
+      } else if (x.Response === 'False') {
+        this.dataTable.data = [];
+        this.errorResponse = x.Error;
+      }
+
     })
+  }
+
+  filterMovies(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.dataTable.filter = inputValue.trim().toLowerCase();
+  }
+
+  sortChange(event: Sort) {
   }
 }
