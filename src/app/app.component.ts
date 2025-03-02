d5bd369d37185a5a2d2174061e1d33aa877517c7
gw0zdziu <gw0zdziu09@gmail.com>
2025-03-02 17:41:43 +0100

    refactor(unused): delete unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import {Component, inject, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
 import {OmdbapiService} from '../shared/apis/omdbapi/omdbapi.service';
-import {JsonPipe, NgOptimizedImage} from '@angular/common';
+import {NgOptimizedImage} from '@angular/common';
 import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {IMovie} from '../shared/models/movie';
@@ -10,17 +9,19 @@ import {
   MatCellDef,
   MatColumnDef,
   MatHeaderCell,
-  MatHeaderCellDef, MatHeaderRow,
-  MatHeaderRowDef, MatRow, MatRowDef,
-  MatTable, MatTableDataSource
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+  MatTableDataSource
 } from '@angular/material/table';
-import {IResponse} from '../shared/models/response';
-import {Target} from '@angular/compiler';
 import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, JsonPipe, MatFormField, MatInput, MatLabel, MatTable, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRow, MatRowDef, MatPaginator, NgOptimizedImage],
+  imports: [MatFormField, MatInput, MatLabel, MatTable, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRow, MatRowDef, MatPaginator, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
